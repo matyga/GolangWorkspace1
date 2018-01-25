7944f128fb6f15cf9d0ca476f9275eaa6e753c63
Robert Matyga <robert.matyga@nokia.com>
2018-01-25 11:58:47 +0100

    third commit

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
-pckage main
- 
- import(
- "fmt"
- "net/http"
- "runtime"
- )
-  
-  func indexHandler( w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
-   
-   }
-    
-    func main(){
-    http.HandleFunc("/", indexHandler)
-    http.ListenAndServe(":8080",nil)
-    }ackage main
- 
+package main
+
 import(
 "fmt"
 "net/http"
 "runtime"
 )
- 
+
 func indexHandler( w http.ResponseWriter, r *http.Request){
 fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
- 
 }
- 
+
 func main(){
 http.HandleFunc("/", indexHandler)
 http.ListenAndServe(":8080",nil)
